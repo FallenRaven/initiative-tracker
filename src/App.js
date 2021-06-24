a6d4891c6b35c75cde7b3eeb2cd00dc1951aee54
matti3 <52759472+FallenRaven@users.noreply.github.com>
2021-06-25 01:42:02 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './Components/InitCard';
+import InitCard from "./Components/InitCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <InitCard></InitCard>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Components/InitCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Card} from "react-bootstrap";
+
+
+function InitCard(){
+    return (
+        <Card>
+            <Card.Body>Harrow Init:16</Card.Body>
+        </Card>
+    );
+
+
+}
+
+export default InitCard
