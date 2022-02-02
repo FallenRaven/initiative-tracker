db5faef7c9bed2ddffe2fb9a1a41baf695a803a4
matti3 <matti3@kth.se>
2022-02-02 19:00:45 +0100

    works now (now with adding features)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import initialData from "./initial-data";
 import {useState} from "react";
 import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 import InitCard from "./Components/InitCard";
-import {Navbar} from "react-bootstrap";
+import {Button, Form, Navbar} from "react-bootstrap";
+import {useFormik} from "formik";
 
 function App() {
 
     const [characters, updateCharacters] = useState(initialData);
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            init:''
+        },
+        onSubmit: values => {
+            const items = Array.from(characters);
+            let tempint = items.length + 1
+            const text = {id: '', name: '', init: ''};
+            text.id = "character-" + tempint
+            text.name = values.name
+            text.init = values.init
+            let tempJSON = JSON.parse(JSON.stringify(text));
+            items.push(tempJSON);
+            updateCharacters(items);
+        },
+    });
 
     function handleOnDragEnd(result) {
         if (!result.destination) return;
@@ -17,43 +36,52 @@ function App() {
         const items = Array.from(characters);
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem);
-
         updateCharacters(items);
+        console.log(characters)
     }
 
-        return (
+    return (
 
-            <div className="App">
-               <header className="App-header">
-
-                       <DragDropContext onDragEnd={handleOnDragEnd}>
-                            <Droppable droppableId="characters">
-                                {(provided) => (
-                                    <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
-                                        {characters.map(({id, name, init}, index) => {
-                                                return (
-                                                    <Draggable key={id} draggableId={id} index={index}>
-                                                        {(provided) => (
-                                                            <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                                <div className="characters-thumb">
-                                                                    <InitCard name={name} init={init} />
-                                                                </div>
-                                                            </li>
-                                                        )}
-                                                    </Draggable>
-                                                );
-                                            })}
-                                            {provided.placeholder}
-                                        </ul>
-                                        )}
-                                            </Droppable>
-                                            </DragDropContext>
-                                            </header>
-                                            <p>
-                                            Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
-                                            </p>
-                                            </div>
-                                            );
+        <div className="App">
+            <header className="App-header">
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <Droppable droppableId="characters">
+                        {(provided) => (
+                            <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+                                {characters.map(({id, name, init}, index) => {
+                                    return (
+                                        <Draggable key={id} draggableId={id} index={index}>
+                                            {(provided) => (
+                                                <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                    <div className="characters-thumb">
+                                                        <InitCard name={name} init={init}/>
+                                                    </div>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    );
+                                })}
+                                {provided.placeholder}
+                            </ul>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+            <Form onSubmit={formik.handleSubmit}>
+                <Form.Group controlId="name">
+                    <Form.Label>Character Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" onChange={formik.handleChange} value={formik.values.name}/>
+                </Form.Group>
+                <Form.Group controlId="init">
+                    <Form.Label>Character Init</Form.Label>
+                    <Form.Control type="text" placeholder="Enter init" onChange={formik.handleChange} value={formik.values.init}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            </header>
+        </div>
+    );
 
 }
 
